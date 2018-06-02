08969a7beafe6c6c04ebc5918bc8eaf87dc940f2
Stan Alam <stan.alam@gmail.com>
2018-06-02 17:06:54 -0400

    added to test.js to use a method that will be creating blockchain from the constructor function, i.e. the CreateBlockChain constructor

--- JS/dev/test.js
+++ JS/dev/test.js
@@ -1,4 +1,5 @@
 const Blockchain = require('./blockchain');
 
 const dogecoin = new Blockchain();
+dogecoin.createNewBlock(1234, '0INA90SDNF90N', '90ANSD9F0N9009N');
 console.log(dogecoin); //cli outtput Blockchain { chain: [], newTransactions: [] }
