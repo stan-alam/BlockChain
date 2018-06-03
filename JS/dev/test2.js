ce7a9abf2022a5785c56f158f95070e2908c0c48
Stan Alam <stan.alam@gmail.com>
2018-06-03 11:56:31 -0400

    updated test2.js to test createNewTransaction, and pushing the said transaction to pending transaction array, recorded terminal output

--- JS/dev/test2.js
+++ JS/dev/test2.js
@@ -6,4 +6,20 @@ dogecoin.createNewBlock(1234, '0INA90SDNF90N', '90ANSD9F0N9009N');
 
 dogecoin.createNewTransaction(100, 'Ben5000200A', 'DarrikDumas50');
 
-console.log(dogecoin); //cli outtput Blockchain { chain: [], newTransactions: [] }
+console.log(dogecoin);
+//terminal output of dogecoin
+/*
+$ node test2.js
+Blockchain {
+  chain:
+   [ { index: 1,
+       timestamp: 1528041331148,
+       transactions: [],
+       nonce: 1234,
+       hash: '90ANSD9F0N9009N',
+       previousBlockHash: '0INA90SDNF90N' } ],
+  pendingTransactions:
+   [ { amount: 100,
+       sender: 'Ben5000200A',
+       recipient: 'DarrikDumas50' } ] }
+*/
